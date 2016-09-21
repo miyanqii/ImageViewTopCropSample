69b92bd1ab12a5ae71d105d1ed4b98f25653027e
Shuhei Miyaki <shufei0327@gmail.com>
2016-09-21 11:05:30 +0900

    Initial commit

---
+++ app/src/main/java/jp/miyanqii/imageviewtopcropsample/TopCropImageView.java
@@ -0,0 +1,38 @@
+package jp.miyanqii.imageviewtopcropsample;
+
+import android.content.Context;
+import android.graphics.Matrix;
+import android.widget.ImageView;
+
+/**
+ * Created by miyaki on 16/09/21.
+ */
+public class TopCropImageView extends ImageView {
+
+    public TopCropImageView(Context context) {
+        super(context);
+        setScaleType(ScaleType.MATRIX);
+    }
+
+    @Override
+    protected boolean setFrame(int l, int t, int r, int b) {
+        final Matrix matrix = getImageMatrix();
+
+        float scale;
+        final int viewWidth = getWidth() - getPaddingLeft() - getPaddingRight();
+        final int viewHeight = getHeight() - getPaddingTop() - getPaddingBottom();
+        final int drawableWidth = getDrawable().getIntrinsicWidth();
+        final int drawableHeight = getDrawable().getIntrinsicHeight();
+
+        if (drawableWidth * viewHeight > drawableHeight * viewWidth) {
+            scale = (float) viewHeight / (float) drawableHeight;
+        } else {
+            scale = (float) viewWidth / (float) drawableWidth;
+        }
+
+        matrix.setScale(scale, scale);
+        setImageMatrix(matrix);
+
+        return super.setFrame(l, t, r, b);
+    }
+}
